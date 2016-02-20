ba3cd54a6878d71ce6aed6c89de8610a38ffbc99
jennylea28 <jenny.lea168@btinternet.com>
2016-02-20 17:38:44 +0000

    still trying

--- chromeEx/background.js
+++ chromeEx/background.js
@@ -1,26 +1,11 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
 	//$("body").append('Test');
 	alert("test");
+	/*chrome.tabs.sendRequest(tab.id, {
+		greeting: 'show hidden'
+	});
+	console.log("sent");*/
 
-	if ( $(".js-discussion .timeline-comment-wrapper").length ) {
-
-    alert("The number of comments is " + $(".js-discussion .js-comment-container").length);
-    $(".js-discussion .js-comment-container").each(function() {
-    	if (($(this).find(".js-comment-body").text().replace(/\s/g,'').length) <= 2) {
-    		//alert($(this).find(".js-comment-body").text());
-    		//alert($(this).find(".js-comment-body").text());
-    		$(this).show();
-    		$(this).css({"border-color": "#FF0000", "border-style":"solid"});
-    		//$(this).hide();
-    	}
-    })
-    	//var text = $('.js-comment-body', el).text();
-    	//alert($('.js-comment-body').text());
-    //})
-
-	} 
-	else {
-		alert ("Cant find any comment");
-	}
-
-});
+    chrome.tabs.executeScript(null,
+        {code:"console.log(\"does this work\")"});
+	});
--- chromeEx/content.js
+++ chromeEx/content.js
@@ -1,34 +1,39 @@
 // Checking page title
-/*document.alert("test");
-if (document.title.indexOf("Google") != -1) {
-    //Creating Elements
-    var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    //Appending to DOM 
-    document.body.appendChild(btn);
-}
-*/
 
 //$("body").append('Test');
 
+var hidden = [];
 
 if ( $(".js-discussion .timeline-comment-wrapper").length ) {
-
     alert("The number of comments is " + $(".js-discussion .js-comment-container").length);
     $(".js-discussion .js-comment-container").each(function() {
     	if (($(this).find(".js-comment-body").text().replace(/\s/g,'').length) <= 2) {
     		//alert($(this).find(".js-comment-body").text());
     		//alert($(this).find(".js-comment-body").text());
     		//$(this).css({"border-color": "#FF0000", "border-style":"solid"});
+    		hidden.push($(this));
     		$(this).hide();
     	}
     })
-    	//var text = $('.js-comment-body', el).text();
-    	//alert($('.js-comment-body').text());
-    //})
-
 } 
 else {
 	alert ("Cant find any comment");
 }
+
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+	console.log(msg, sender, sendResponse);
+    /* If the received message has the expected format... */
+    if (msg.greeting) {
+        alert('Received a msg from bg...')
+        //sendResponse('hey_bp');
+        for (var i = 0; i < hidden.length; i++) {
+        	alert("looking at");
+    		(hidden[i]).show();
+    	//Do something
+		}
+		//location.reload();
+    }
+});
+
+chrome.tabs.executeScript(null,
+        {code:"console.log(\"does this work\")"});
